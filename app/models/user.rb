2b51501f81e7ce4a353c31e17f15840c5da01287
dr122 <n09013304@gmail.com>
2024-06-05 13:27:41 +0900

    バリデーション、エラーメッセージの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を入力してください' } do
+    validates :first_name, message: '全角文字を入力してください'
+    validates :last_name, message: '全角文字を入力してください'
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナを入力してください' } do
+    validates :first_name_kana, message: '全角カタカナを入力してください'
+    validates :last_name_kana, message: '全角カタカナを入力してください'
+  end
 end
