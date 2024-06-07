19023e10728a0a2ec76002a5c2c0e421a2a89424
dr122 <n09013304@gmail.com>
2024-06-07 18:16:18 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_from
+  belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :shipping_from_id
+    validates :shipping_day_id
+  end
 end
