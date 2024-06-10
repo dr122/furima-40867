304de65327d4444a83220320982addc870d00891
dr122 <n09013304@gmail.com>
2024-06-10 12:28:14 +0900

    商品出品ページ表示、商品情報保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  
+s
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -10,16 +10,17 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :description
-    validates :price
+    validates :price, numericality: true
 
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id
       validates :condition_id
       validates :shipping_fee_id
       validates :shipping_from_id
-      validates :shipping_day_id
+      validates :shipping_days_id
     end
   end
 end
